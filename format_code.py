af1b693c855c50d99fd53c7bc634a2091a6a0dbc
yangxuxin <18729346082@163.com>
2022-05-06 15:11:07 +0800

    Update format_code.py, change stand_code.cpp by py, so that we just copy our stand code to stand_code.cpp

--- format_code.py
+++ format_code.py
@@ -3,11 +3,52 @@
     Format gen_input.cpp and stand_code.cpp
 """
 
-def read_file(file_name):
-    pass
+def read_file(file_name) -> str:
+    if file_name == "gen_input.cpp":
+        pass
+    elif file_name == "stand_code.cpp":
+        with open(file_name, "r", encoding="utf-8") as f:
+            lines = f.readlines()
+    return lines
 
-def write_file(file_name, content):
-    pass
+def write_file(file_name, content) -> bool:
+    flag = False
+    if file_name == "gen_input.cpp":
+        pass
+    elif file_name == "stand_code.cpp":
+        with open(file_name, "w", encoding="utf-8") as f:
+            f.writelines(content)
+            flag = True
+    return flag
+
+def format_code(content) -> str:
+    start_curly = 0
+    for line in range(len(content)):
+        # Find function main
+        if "int main()" in content[line]:
+            print("--------------------------Processing main function--------------------------")
+            content[line] = content[line].replace("int main()", "int main(int argc, char *argv[])")
+            start_curly = 1
+        if start_curly == 1 and "{" in content[line]:
+            content[line] += "\tstring filename = argv[1];\n"
+            content[line] += "\tstring outfilename = argv[2];\n"
+            content[line] += "\tfreopen(filename.c_str(), \"r\", stdin);\n"
+            content[line] += "\tfreopen(outfilename.c_str(), \"w\", stdout);\n"
+            break
+    return content
+
+        
 
 if __name__ == '__main__':
-    pass
+    # Process gen_input.cpp
+
+    # Process stand_code.cpp
+    input_name = 'stand_code.cpp'
+    output_name = 'stand_code.cpp'
+    input_content = read_file(input_name)
+    output_content = format_code(input_content)
+    flag = write_file(output_name, output_content)
+    if flag:
+        print("File Writed Successful!")
+    else:
+        print("File Writed Failed!")
--- gen_input.cpp
+++ gen_input.cpp
@@ -4,14 +4,18 @@ using namespace std;
 
 int main(int argc, char **argv)
 {
-    registerGen(argc, argv, 1); // register the random generator function
-    for (int i = 1; i <= 10; i++) // generate 10 test cases
+    registerGen(argc, argv, 1);    // register the random generator function
+    for (int i = 1; i <= 100; i++) // generate 10 test cases
     {
         startTest(i);
         // 创造一个含元音字母的随机字符串，长度在1-3*10^5之间
-        string s;
-        int len = rnd.next(1, 10);
-        cout << rnd.next("[0-9][1-9]{1,%d}", len) << endl;
+        int n = rnd.next(1, 1000);
+        printf("%d\n", n);
+        for (int i = 1; i <= n; i++)
+        {
+            int x = rnd.next(1, 100);
+            printf("%d ", x);
+        }
     }
     return 0;
 }
